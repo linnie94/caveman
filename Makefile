b1a20d698a90a5d5fc762dcd021aa76fb94a249a
lhrs94 <lhrsohn94@gmail.com>
2018-03-27 21:01:26 -0700

    created window + delay 10s

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	clang *.c -std=c99 -Wall -Wextra -lSDL2 -o explosion
+	clang *.c -std=c99 -Wall -Wextra -lSDL2 -o caveman
--- main.c
+++ main.c
@@ -3,5 +3,14 @@
 
 int main()
 {
-
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    int xres = 800;
+    int yres = 400;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(xres, yres, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
 }
