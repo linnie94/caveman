884d71c4324eb0b5dcaac96148beca36535ecbd5
lhrs94 <lhrsohn94@gmail.com>
2018-03-30 15:07:31 -0700

    playing with rand() for RGB

--- main.c
+++ main.c
@@ -13,8 +13,6 @@ int main()
     int done = 0;
     while(!done)
     {
-        int t1;
-        t1 = SDL_GetTicks();
         SDL_PollEvent(&event);
         key = SDL_GetKeyboardState(NULL);
         if(event.type == SDL_QUIT || key[SDL_SCANCODE_ESCAPE] || key[SDL_SCANCODE_END])
@@ -22,17 +20,8 @@ int main()
             done = 1;
         }
 
-        Util_QuickFill(renderer, 255, 255, 0);
+        Util_QuickFill(renderer, rand() % 256, rand() % 256, rand() % 256);
         SDL_Delay(60);
-        Util_QuickFill(renderer, 255, 0, 0);
-        SDL_Delay(60);
-        Util_QuickFill(renderer, 0, 255, 0);
-        SDL_Delay(60);
-        Util_QuickFill(renderer, 0, 0, 255);
-        SDL_Delay(60);
-        int t2;
-        t2 = SDL_GetTicks();
-        printf("%d\n", t2 - t1);
     }
 
 }
