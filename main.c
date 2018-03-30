650b11aed64ac760b62dd0335f5e17c5b4802823
lhrs94 <lhrsohn94@gmail.com>
2018-03-30 13:50:57 -0700

    while loop

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-//#include <stdio.h>
-//#include <SDL2/SDL.h>
 #include "Util.h"
 
 int main()
@@ -8,15 +6,27 @@ int main()
     SDL_Renderer* renderer;
     int xres = 800;
     int yres = 400;
+    const uint8_t* key;
+    SDL_Event event;
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(xres, yres, 0, &window, &renderer);
-    Util_QuickFill(renderer, 255, 255, 0);
-    SDL_Delay(3000);
-    Util_QuickFill(renderer, 255, 0, 0);
-    SDL_Delay(3000);
-    Util_QuickFill(renderer, 0, 255, 0);
-    SDL_Delay(3000);
-    Util_QuickFill(renderer, 0, 0, 255);
-    SDL_Delay(3000);
+    int done = 0;
+    while(!done)
+    {
+        SDL_PollEvent(&event);
+        key = SDL_GetKeyboardState(NULL);
+        if(event.type == SDL_QUIT || key[SDL_SCANCODE_ESCAPE] || key[SDL_SCANCODE_END])
+        {
+            done = 1;
+        }
 
+        Util_QuickFill(renderer, 255, 255, 0);
+        SDL_Delay(1500);
+        Util_QuickFill(renderer, 255, 0, 0);
+        SDL_Delay(1500);
+        Util_QuickFill(renderer, 0, 255, 0);
+        SDL_Delay(1500);
+        Util_QuickFill(renderer, 0, 0, 255);
+        SDL_Delay(1500);
+    }
 }
