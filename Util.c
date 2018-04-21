9c6f8a878f4145123332e807718924e3ee8a07ad
Lynn Hyerin Sohn <lhrsohn94@gmail.com>
2018-04-20 20:31:38 -0700

    Update Util.c

--- Util.c
+++ Util.c
@@ -6,6 +6,15 @@ void Util_QuickFill(SDL_Renderer* renderer, int r, int g, int b)
     SDL_RenderClear(renderer);
 }
 
+void check(void* p)
+{
+    if(p == NULL)
+    {
+        printf(SDL_GetError()\n);
+        exit(1);
+    }
+}
+
 SDL_Surface* load(const char* path, uint32_t r, uint32_t g, uint32_t b)
 {
     // Loads surface from bmp image.
