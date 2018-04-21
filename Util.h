347b6d43d1a36d2637c0d76f51eacbff2d8bf89a
Lynn Hyerin Sohn <lhrsohn94@gmail.com>
2018-04-21 12:15:41 -0700

    Update Util.h

--- Util.h
+++ Util.h
@@ -5,4 +5,4 @@
 
 void Util_QuickFill(SDL_Renderer* renderer, int r, int g, int b);
 
-SDL_Surface* load(const char* path, uint32_t r, uint32_t g, uint32_t b);
+SDL_Surface* Util_load(const char* path, uint32_t r, uint32_t g, uint32_t b);
