6b59118b3788bfcc1480867f59c9f18c885acf56
lhrs94 <lhrsohn94@gmail.com>
2018-05-02 20:04:02 -0700

    effort to move ghost into function

--- main.c
+++ main.c
@@ -32,6 +32,57 @@ void draw_grass(int xres, int yres, SDL_Renderer* renderer, SDL_Texture* texture
     }
 }
 
+void draw_ghost(uint8_t* key, float vx, float vy, SDL_Renderer* renderer, SDL_Texture* texture)
+{
+    SDL_Rect from_g;
+    from_g.x = 96;
+    from_g.y = 96;
+    from_g.w = 16;
+    from_g.h = 16;
+    SDL_Rect to_g;
+    to_g.x = 16;
+    to_g.y = 16;
+    to_g.w = 32;
+    to_g.h = 32;
+    if(vx > 0)
+    {
+        to_g.x += vx;
+    }
+    else
+    {
+        to_g.x -= vx;
+    }
+    if(vy > 0)
+    {
+        to_g.y += vy;
+    }
+    else
+    {
+        to_g.y -= vy;
+    }
+    if(key[SDL_SCANCODE_D])
+    {
+        vx += 1;
+        from_g.y = 96;
+    }
+    if(key[SDL_SCANCODE_A])
+    {
+        vx -= 1;
+        from_g.y = 80;
+    }
+    if(key[SDL_SCANCODE_W])
+    {
+        vy -= 1;
+        from_g.y = 112;
+    }
+    if(key[SDL_SCANCODE_S])
+    {
+        vy += 1;
+        from_g.y = 64;
+    }
+    SDL_RenderCopy(renderer, texture_c, &from_g, &to_g);
+}
+
 int main()
 {
     SDL_Window* window;
@@ -43,8 +94,8 @@ int main()
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindowAndRenderer(xres, yres, 0, &window, &renderer);
     SDL_Surface* surface = Util_load("art/basic.bmp", 255, 255, 255);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_Surface* surface_c = Util_load("art/chars.bmp", 255, 255, 255);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_Texture* texture_c = SDL_CreateTextureFromSurface(renderer, surface_c);
     if(surface == NULL || texture == NULL)
     {
@@ -71,53 +122,7 @@ int main()
         Util_QuickFill(renderer, 0, 0, 0);
         draw_grass(xres, yres, renderer, texture);
         // Ghost
-        SDL_Rect from_g;
-        from_g.x = 96;
-        from_g.y = 96;
-        from_g.w = 16;
-        from_g.h = 16;
-        SDL_Rect to_g;
-        to_g.x = 16;
-        if(vx > 0)
-        {
-            to_g.x += vx;
-        }
-        else
-        {
-            to_g.x -= vx;
-        }
-        to_g.y = 16;
-        if(vy > 0)
-        {
-            to_g.y += vy;
-        }
-        else
-        {
-            to_g.y -= vy;
-        }
-        to_g.w = 32;
-        to_g.h = 32;
-        if(key[SDL_SCANCODE_D])
-        {
-            vx += 1;
-            from_g.y = 96;
-        }
-        if(key[SDL_SCANCODE_A])
-        {
-            vx -= 1;
-            from_g.y = 80;
-        }
-        if(key[SDL_SCANCODE_W])
-        {
-            vy -= 1;
-            from_g.y = 112;
-        }
-        if(key[SDL_SCANCODE_S])
-        {
-            vy += 1;
-            from_g.y = 64;
-        }
-        SDL_RenderCopy(renderer, texture_c, &from_g, &to_g);
+        draw_ghost(key, vx, vy, renderer, texture_c);
         SDL_RenderPresent(renderer);
         const int t2 = SDL_GetTicks();
         const int ms = 1000.0 / (60 - (t2 - t1));
