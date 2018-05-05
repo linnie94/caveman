226dafa265f3e7ad309b1cf0e4c8759c4e4b16b1
lhrs94 <lhrsohn94@gmail.com>
2018-05-04 21:06:12 -0700

    Ghost animation complete

--- main.c
+++ main.c
@@ -32,25 +32,85 @@ void draw_grass(int xres, int yres, SDL_Renderer* renderer, SDL_Texture* texture
     }
 }
 
-void draw_ghost(uint8_t* key, SDL_Renderer* renderer, SDL_Texture* texture_c, SDL_Rect* from_g, SDL_Rect* to_g)
+void draw_ghost(int s1, uint8_t* key, SDL_Rect* from_g, SDL_Rect* to_g)
 {
     if(key[SDL_SCANCODE_D])
     {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 96;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 96;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 96;
+        }
         to_g->x += 1;
         from_g->y = 96;
     }
     if(key[SDL_SCANCODE_A])
     {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 80;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 80;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 80;
+        }
         to_g->x -= 1;
         from_g->y = 80;
     }
     if(key[SDL_SCANCODE_W])
     {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 112;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 112;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 112;
+        }
         to_g->y -= 1;
         from_g->y = 112;
     }
     if(key[SDL_SCANCODE_S])
     {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 64;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 64;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 64;
+        }
         to_g->y += 1;
         from_g->y = 64;
     }
@@ -94,6 +154,7 @@ int main()
     while(!done)
     {
         const int t1 = SDL_GetTicks();
+        int s1 = (t1/250) % 16;
         // Exit
         SDL_PollEvent(&event);
         key = SDL_GetKeyboardState(NULL);
@@ -105,7 +166,7 @@ int main()
         Util_QuickFill(renderer, 0, 0, 0);
         draw_grass(xres, yres, renderer, texture);
         // Ghost
-        draw_ghost(key, renderer, texture_c, &from_g, &to_g);
+        draw_ghost(s1, key, &from_g, &to_g);
         SDL_RenderCopy(renderer, texture_c, &from_g, &to_g);
         SDL_RenderPresent(renderer);
         const int t2 = SDL_GetTicks();
