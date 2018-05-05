1d9a5ead165147f2c8fecf93f3182bdda6a105a6
lhrs94 <lhrsohn94@gmail.com>
2018-05-04 21:29:29 -0700

    Ghost animation in stationary position

--- main.c
+++ main.c
@@ -54,6 +54,24 @@ void draw_ghost(int s1, uint8_t* key, SDL_Rect* from_g, SDL_Rect* to_g)
         to_g->x += 1;
         from_g->y = 96;
     }
+    else if(from_g->y == 96)
+    {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 96;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 96;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 96;
+        }
+    }
     if(key[SDL_SCANCODE_A])
     {
         if(s1 % 3 == 1)
@@ -73,6 +91,24 @@ void draw_ghost(int s1, uint8_t* key, SDL_Rect* from_g, SDL_Rect* to_g)
         }
         to_g->x -= 1;
         from_g->y = 80;
+    }
+    else if(from_g->y == 80)
+    {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 80;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 80;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 80;
+        }
     }
     if(key[SDL_SCANCODE_W])
     {
@@ -94,6 +130,24 @@ void draw_ghost(int s1, uint8_t* key, SDL_Rect* from_g, SDL_Rect* to_g)
         to_g->y -= 1;
         from_g->y = 112;
     }
+    else if(from_g->y == 112)
+    {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 112;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 112;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 112;
+        }
+    }
     if(key[SDL_SCANCODE_S])
     {
         if(s1 % 3 == 1)
@@ -113,6 +167,24 @@ void draw_ghost(int s1, uint8_t* key, SDL_Rect* from_g, SDL_Rect* to_g)
         }
         to_g->y += 1;
         from_g->y = 64;
+    }
+    else if(from_g->y == 64)
+    {
+        if(s1 % 3 == 1)
+        {
+            from_g->x = 96;
+            from_g->y = 64;
+        }
+        else if(s1 % 3 == 2)
+        {
+            from_g->x = 112;
+            from_g->y = 64;
+        }
+        else if(s1 % 3 == 3)
+        {
+            from_g->x = 128;
+            from_g->y = 64;
+        }
     }
 }
 
