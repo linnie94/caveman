66075d9fdcf1c835d6b72baa10bcaf3c76a01a75
lhrs94 <lhrsohn94@gmail.com>
2018-05-20 17:48:47 -0700

    ...

--- main.c
+++ main.c
@@ -121,7 +121,10 @@ int main()
         Map_Draw(map, xres, yres, renderer, texture);
         Map_Draw(map_obj, xres, yres, renderer, texture);
         // Portalsd
-        printf("%d\n", portal_location(portals, &to_g));
+        if(portal_location(portals, &to_g) != -1)
+        {
+            printf("%d\n", portal_location(portals, &to_g));
+        }
         // Ghost
         Ghost_Draw(s1, key, &from_g, &to_g);
         SDL_RenderCopy(renderer, texture_c, &from_g, &to_g);
